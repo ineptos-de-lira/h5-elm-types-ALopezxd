3f5d17f728b208d23841ed7c7e88ad11ca77e003
ALopezxd <joseamau26@gmail.com>
2026-02-26 16:14:59 -0600

    added types excercises tested and formatted

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,51 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map
+        (\grade ->
+            if grade >= 7 then
+                Approved
+
+            else if grade < 0 then
+                Pending
+
+            else
+                Failed
+        )
+        grades
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction domain =
+    case domain of
+        OnTime ->
+            "Esperar"
+
+        Delayed ->
+            "Esperar"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
